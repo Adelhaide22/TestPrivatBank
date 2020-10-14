9d13878b7f61fc3ca622c0e36cfa02cdd9b16e59
adelhaide <adelhaide22@gmail.com>
2020-10-14 12:49:29 +0300

    Extend Worker.cs

--- Worker/Worker.cs
+++ Worker/Worker.cs
@@ -14,42 +14,85 @@ namespace Worker
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IRepository _repository;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IRepository repository)
         {
             _logger = logger;
+            _repository = repository;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             var factory = new ConnectionFactory {HostName = "localhost"};
             using var connection = factory.CreateConnection();
-            using var channel = connection.CreateModel();
-            
-            channel.QueueDeclare(queue: "AddApplicationQueue",
-                durable: false,
-                exclusive: false,
-                autoDelete: false,
-                arguments: null);
 
-            var consumer = new EventingBasicConsumer(channel);
-            consumer.Received += (model, ea) =>
-            {
-                var body = ea.Body.ToArray();
-                var message = Encoding.UTF8.GetString(body);
-                var command = JsonConvert.DeserializeObject<AddApplicationMqCommand>(message);
-            };
-            channel.BasicConsume(queue: "AddApplicationQueue",
-                autoAck: true,
-                consumer: consumer);
+            var addCallback = GetAddCommand(new AddApplicationMqCommand());
+            ReceiveMessage(connection, "AddApplicationQueue", GetAddCommand(new AddApplicationMqCommand()));
+            var addCommand = addCallback.Item2;
+            _repository.AddApplication(addCommand);
 
-            
+            var getByRequestCallback = GetGetByRequestCommand(new GetApplicationByRequestIdMqCommand());
+            ReceiveMessage(connection, "GetByRequestQueue", getByRequestCallback);
+            var getByRequestCommand = getByRequestCallback.Item2;
+            _repository.GetApplicationByRequestId(getByRequestCommand);
+
+            var getByClientCallback = GetGetByClientCommand(new GetApplicationByClientIdMqCommand());
+            ReceiveMessage(connection, "GetByClientQueue", getByClientCallback);
+            var getByClientCommand = getByClientCallback.Item2;
+            _repository.GetApplicationByClientId(getByClientCommand);
             
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(1000, stoppingToken);
             }
+        }
+        private static void ReceiveMessage(IConnection connection, string queryName, (EventHandler<BasicDeliverEventArgs>, ICommand) callback)
+        {
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: queryName,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += callback.Item1;
+            channel.BasicConsume(queue: "AddApplicationQueue",
+                autoAck: true,
+                consumer: consumer);
+        }
+
+        private static (EventHandler<BasicDeliverEventArgs>, AddApplicationMqCommand) GetAddCommand(AddApplicationMqCommand addCommand)
+        {
+            return ((model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                addCommand = JsonConvert.DeserializeObject<AddApplicationMqCommand>(message);
+            }, addCommand);
+        }
+        
+        private static (EventHandler<BasicDeliverEventArgs>, GetApplicationByRequestIdMqCommand) GetGetByRequestCommand(GetApplicationByRequestIdMqCommand getByRequestCommand)
+        {
+            return ((model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                getByRequestCommand = JsonConvert.DeserializeObject<GetApplicationByRequestIdMqCommand>(message);
+            }, getByRequestCommand);
+        }
+
+        private static (EventHandler<BasicDeliverEventArgs>, GetApplicationByClientIdMqCommand) GetGetByClientCommand(GetApplicationByClientIdMqCommand getByClientCommand)
+        {
+            return ((model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                getByClientCommand = JsonConvert.DeserializeObject<GetApplicationByClientIdMqCommand>(message);
+            }, getByClientCommand);
         }
     }
 }
